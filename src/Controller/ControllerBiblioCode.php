532f575c911d1712637db6249611b667321fd99d
lauraaa96 <laura96_3@hotmail.com>
2022-03-02 19:04:55 +0100

    Cambios laura

---
+++ src/Controller/ControllerBiblioCode.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+use App\Entity\Tutorial;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ControllerBiblioCode extends AbstractController
+{
+//VER NOTICIA
+    function verFormulario()
+    {
+         return $this->render('formularioContacto.html.twig'); 
+
+    }
+
+    function verIndex()
+    {
+         return $this->render('index.html.twig'); 
+
+    }
+}
+
+?>
