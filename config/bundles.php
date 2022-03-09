d2b61dcd8cc0213c4d2d6317ba12abeb5d05b3ca
AlbertoArraz <alberto4arraz@gmail.com>
2022-03-09 09:51:02 +0100

    Login de google funcionando

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
