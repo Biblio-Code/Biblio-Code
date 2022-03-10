46d7aac94ad8af43ad5c28decb1a9aa95941ceb4
AlbertoArraz <alberto4arraz@gmail.com>
2022-03-10 10:08:25 +0100

    API terminada

--- src/Controller/TutorialController.php
+++ src/Controller/TutorialController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 // /src/Controller/TutorialController.php
@@ -17,44 +18,44 @@ class TutorialController extends AbstractController
     {
         $tutorial = $dm->getRepository(Tutorial::class)->find($id);
 
-    if (! $tutorial) {
-        throw $this->createNotFoundException('No product found for id ' . $id);
-    }
+        if (!$tutorial) {
+            throw $this->createNotFoundException('No product found for id ' . $id);
+        }
 
-    $result = new \stdClass();
-    $result->id = $tutorial->getId();
-    $result->titulo = $tutorial->getTitulo();
-    $result->codigo = $tutorial->getCodigo();
-    $result->texto = $tutorial->getTexto();
+        $result = new \stdClass();
+        $result->id = $tutorial->getId();
+        $result->titulo = $tutorial->getTitulo();
+        $result->codigo = $tutorial->getCodigo();
+        $result->texto = $tutorial->getTexto();
 
-    return new JsonResponse($result);
+        return new JsonResponse($result);
     }
 
     public function getAllTutorial(DocumentManager $dm)
     {
         $tutoriales = $dm->getRepository(Tutorial::class)->findAll();
 
-    if (! $tutoriales) {
-        throw $this->createNotFoundException('No Tutorial found');
+        if (!$tutoriales) {
+            throw $this->createNotFoundException('No Tutorial found');
+        }
+
+        $results = new \stdClass();
+        $results->count = count($tutoriales);
+        $results->results = array();
+
+        foreach ($tutoriales as $tutorial) {
+            $result = new \stdClass();
+            $result->id = $tutorial->getId();
+            $result->url = $this->generateUrl('getTutorial', [
+                'id' => $result->id,
+            ], UrlGeneratorInterface::ABSOLUTE_URL);
+
+            array_push($results->results, $result);
+        }
+
+        return new JsonResponse($results);
     }
 
-    $results = new \stdClass();
-    $results->count = count($tutoriales);
-    $results->results = array();
-
-    foreach ($tutoriales as $tutorial) {
-        $result = new \stdClass();
-        $result->id = $tutorial->getId();
-        $result->url = $this->generateUrl('getTutorial', [
-          'id' => $result->id,
-        ], UrlGeneratorInterface::ABSOLUTE_URL);
-  
-        array_push($results->results, $result);
-      }
-
-    return new JsonResponse($result);
-    }
-    
     public function postTutorial(DocumentManager $dm, Request $request)
     {
         $tutorial = new Tutorial();
@@ -73,7 +74,41 @@ class TutorialController extends AbstractController
         return new JsonResponse($result, 201);
     }
 
-    
+    function putTutorial(DocumentManager $dm, Request $request, $id)
+    {
+        $tutorial = $dm->getRepository(Tutorial::class)->find($id);
+        if ($tutorial == null) {
+            return new JsonResponse([
+                'error' => 'Tutorial not found'
+            ], 404);
+        }
+        $tutorial->setTitulo($request->request->get("titulo"));
+        $tutorial->setLenguaje($request->request->get("lenguaje"));
+        $tutorial->setTexto($request->request->get("texto"));
+        $tutorial->setCodigo($request->request->get("codigo"));
+        $dm->flush();
+        $result = new \stdClass();
+        $result->id = $tutorial->getId();
+        $result->titulo = $tutorial->getTitulo();
+        $result->lenguaje = $tutorial->getLenguaje();
+        $result->texto = $tutorial->getTexto();
+        $result->codigo = $tutorial->getCodigo();
+        return new JsonResponse($result);
+    }
 
+    function deleteTutorial(DocumentManager $dm, Request $request, $id)
+  {
+    $tutorial = $dm->getRepository(Tutorial::class)->find($id);
+    if ($tutorial == null) {
+      return new JsonResponse([
+        'error' => 'Tutorial not found'
+      ], 404);
+    }
 
+    $dm->remove($tutorial);
+    $dm->flush();
+
+    // Devuelve una respuesta vacia
+    return new JsonResponse(null, 204);
+  }
 }
