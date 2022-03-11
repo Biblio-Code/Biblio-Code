f69b8f3e0b330da158a71f1b0f7b276100448a48
lauraaa96 <laura96_3@hotmail.com>
2022-03-11 22:57:40 +0100

    prueba

--- src/Controller/BiblioCodeController.php
+++ src/Controller/BiblioCodeController.php
@@ -4,6 +4,7 @@ use App\Entity\Tutorial;
 use App\Entity\Comunidad;
 use App\Entity\Provincia;
 use App\Entity\Municipio;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\Persistence\ManagerRegistry;
@@ -37,5 +38,54 @@ class BiblioCodeController extends AbstractController
         $tutoriales = $dm->getRepository(Tutorial::class)->findAll();
         return $this->render('index.html.twig', ['tutoriales' => $tutoriales]); 
     }
+
+    function verTablaTutoriales(ManagerRegistry $doctrine)
+    {
+        $tutoriales = $doctrine->getRepository(Tutorial::class)->findAll();
+
+        if (!$tutoriales) {
+            throw $this->createNotFoundException('No Tutorial found');
+        }
+
+        $results = new \stdClass();
+        $results->count = count($tutoriales);
+        $results->results = array();
+
+        foreach ($tutoriales as $tutorial) {
+            $result = new \stdClass();
+            $result->id = $tutorial->getId();
+            $result->titulo = $tutorial->getTitulo();
+            $result->lenguaje = $tutorial->getLenguaje();
+
+            array_push($results->results, $result);
+        }
+
+        return $this->render('tablaTutoriales.html.twig', array('tutoriales' => $tutoriales)); 
+
+     }
+
+     function jsonTutoriales(ManagerRegistry $doctrine)
+    {
+        $tutoriales = $doctrine->getRepository(Tutorial::class)->findAll();
+
+        if (!$tutoriales) {
+            throw $this->createNotFoundException('No Tutorial found');
+        }
+
+        $data = new \stdClass();
+        $data->data = array();
+
+        foreach ($tutoriales as $tutorial) {
+            $result = new \stdClass();
+            $result->id = $tutorial->getId();
+            $result->titulo = $tutorial->getTitulo();
+            $result->lenguaje = $tutorial->getLenguaje();
+
+            array_push($data->data, $result);
+        }
+
+        return new JsonResponse($data);
+
+     }
 }
 
